5faf4482184e8e5166e69d71b10401727c114821
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2023-11-11 21:34:43 +0700

    Câu 0 và Câu 1

---
+++ Calculator/CalculatorTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTester")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6f495d0-2691-49a2-871b-1c13310961e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator/CalculatorTester/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
+
+namespace CalculatorTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\TestData.csv", "TestData#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestWithDataSource()
+        {
+            int a = int.Parse(TestContext.DataRow[0].ToString());
+            int b = int.Parse(TestContext.DataRow[1].ToString());
+            int expected = int.Parse(TestContext.DataRow[2].ToString());    
+
+            Calculation c = new Calculation(a, b);
+            int actual = c.Execute("+");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\Data\DataTest_cotToantu.csv", "DataTest_cotToantu.csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestCong_DataSource()
+        {
+            int a = int.Parse(TestContext.DataRow[0].ToString());
+            int b = int.Parse(TestContext.DataRow[1].ToString());
+            string phepToan = TestContext.DataRow[2].ToString();
+            int expected = int.Parse(TestContext.DataRow[3].ToString());
+
+            Calculation c = new Calculation(a, b);
+            int actual = c.Execute(phepToan);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
